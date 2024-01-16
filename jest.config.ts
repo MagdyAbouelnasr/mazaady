548256e1663fc4221cccb7043b87d6e075372db0
MagdyAbouelnasr <mmabouelnasr@gmail.com>
2024-01-16 20:24:00 +0200

    align test env to next build process

--- jest.config.ts
+++ jest.config.ts
@@ -179,9 +179,8 @@ const config: Config = {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    "^.+\\.(t|j)sx?$": "@swc/jest",
   },
-
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "\\\\node_modules\\\\",
